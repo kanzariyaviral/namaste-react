680100abd7171d72d2a43c06744e086ed9e2e8de
viral kanzariya <kanzariyaviral2312002@gmail.com>
2024-04-02 16:25:30 +0530

    add eslint and prettier

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -5,33 +5,28 @@ const MenuCard = (props) => {
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABlUlEQVR4nO2XwUoDQQyG56be1KMVH8OJTWSvfQq1+g4iFfEoQpPaB7H2Naqox1a8qXdb6r2SrR6LdXd2Zkvnh8DC7sB8m0nmjzFRUVFRpRQITcoUZmkBTGBBBAgsiBlYpgwAw+ZeC4+BsWuZBiD4paHPlvHOMtXpijZKBwAMa7ZJ58A0+rsl4hCEGrrGlAGgel3dskz3/+3rlukZb3AnKMBuO9m2Qh+ZLyjGd2CoBAFIjw3jY+5blukhuUxWvQNYwQtXNsEKnnkF0G4zX8HOGzic1Z0KAsATd5unaRaa+0c+AbrOAYQ63gCs0KtzAKaBPwDGsXsAHC80ADCNvAGA4EsBNdD3BqDGrIAM3HoEoLp7ADzwe5FNXaWrAv5MWsm6N4Cf9w2HGTjNuo/MAGrA1IjlL17sBTFz6TcMldQSZwbAN50n8u4j/0Aj2Mvw55+CDzS/0iOglniewtaC1TNfa9dWXO8j90K1xOoq0wFeqK83dhr6LNQBwcNZ3aYUAEUIIkBgQczAomcAShJm6QCioqKijA99A3yCPIRsuFUZAAAAAElFTkSuQmCC";
   const nonVegBase64 =
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABoklEQVR4nO2Xv0oDQRDGtzOXmRi1VLGyt5AZgwh5E/++g4jiIxmtRNTsFIKgopZG7Iy9CbFX5gx2wSS32b2Q/WDgiluY383OzHfGREVFReVSwvidpzATC2ACSyJAYEmswCRV4KIyPVfn0q4QnAlDQxi+utGwBKeyVtq52SjP5g7gtmISS3gkBO3/RyK0LOOhnjF5ALhcT+Yt4/2gc90yPl+tFpaCAggni0L4MexysoTN60qyEARAr4AQPmbdsJbxQaqm4B1ACI+dWQXCA68AOm36a9h+A1q9ptNIACzjnrvksVuF0rY3gN857xbAMtS8AViCN+cVYGj4BOg4rwBBZ6wBhKDtDUAYXkfQAy/eAFJj5r4HTrwBqKt0DVDn4qbfRcbQcnh9PmWlPOMNQKWW2B0A7g+bx9AAasBSI5b97t8FMXMqtcJqiTN8+Xf9n8iaR6aDmkD6FQdP/in4D83fu1VTUEvcT2Nrw+qdP182U67zyHxQLbG6St0Tuph0Y6ehzww1S8WtXtMmFwCjkESAwJJYgXGvgOQkzMQBREVFRRkf+gHhzjmciMgUEwAAAABJRU5ErkJggg==";
+
+  const { name, price, defaultPrice, description, imageId,isVeg } =
+    props.data.card.info;
+
   return (
     <div className="menu-card">
       <div className="menu-card-detail">
         <img
           className="veg-non-img"
           alt="No Image"
-          src={props?.data?.card?.info?.isVeg ? vegBase64 : nonVegBase64}
+          src={isVeg ? vegBase64 : nonVegBase64}
         ></img>
-        <h3>{props.data.card.info.name}</h3>
-        <h5>
-          ₹{" "}
-          {(props?.data?.card?.info?.price
-            ? props?.data?.card?.info?.price
-            : props?.data?.card?.info?.defaultPrice) / 100}
-        </h5>
-        <p style={{ color: "grey" }}>
-          {props?.data?.card?.info?.description
-            ? props?.data?.card?.info?.description
-            : null}
-        </p>
+        <h3>{name}</h3>
+        <h5>₹ {(price ? price : defaultPrice) / 100}</h5>
+        <p style={{ color: "grey" }}>{description ? description : null}</p>
       </div>
-      {props?.data?.card?.info?.imageId ? (
+      {imageId ? (
         <div>
           <img
             className="menu-logo"
             alt="No Image"
-            src={`${LOGO_RESTATURANT_MENU}${props.data.card.info.imageId}`}
+            src={`${LOGO_RESTATURANT_MENU}${imageId}`}
           ></img>
         </div>
       ) : (
